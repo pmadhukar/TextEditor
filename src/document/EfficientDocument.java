2eea3e4fcda33fd414c5f25cc7f9beb3b1ea8428
Pratyush Madhukar <pratyush.madhukar@gmail.com>
2016-02-13 15:20:40 +0530

    Implemented methods in EfficientDocument.java

--- src/document/EfficientDocument.java
+++ src/document/EfficientDocument.java
@@ -2,11 +2,11 @@ package document;
 
 import java.util.List;
 
-/** 
+/**
  * A class that represents a text document
- * It does one pass through the document to count the number of syllables, words, 
+ * It does one pass through the document to count the number of syllables, words,
  * and sentences and then stores those values.
- * 
+ *
  * @author UC San Diego Intermediate Programming MOOC team
  */
 public class EfficientDocument extends Document {
@@ -14,14 +14,14 @@ public class EfficientDocument extends Document {
 	private int numWords;  // The number of words in the document
 	private int numSentences;  // The number of sentences in the document
 	private int numSyllables;  // The number of syllables in the document
-	
+
 	public EfficientDocument(String text)
 	{
 		super(text);
 		processText();
 	}
-	
-	
+
+
 	/** Return true if this string is a word (as opposed to punctuation)
 	 * @param tok The string to check
 	 * @return true if tok is a word, false otherwise. */
@@ -31,28 +31,66 @@ public class EfficientDocument extends Document {
 	    // You probably don't want to change it.
 		return !(tok.indexOf("!") >=0 || tok.indexOf(".") >=0 || tok.indexOf("?")>=0);
 	}
-	
-	
-    /** Passes through the text one time to count the number of words, syllables and 
+
+
+    /** Passes through the text one time to count the number of words, syllables and
      * sentences, and set the member variables appropriately.
-     * Words, sentences and syllables are defined as described below. 
+     * Words, sentences and syllables are defined as described below.
      */
 	private void processText()
 	{
-		// Provide this first line in the starter code.  
+		// Provide this first line in the starter code.
 		// Words are only strings of letters.  No numbers.
 		List<String> tokens = getTokens("[!?.]+|[a-zA-Z]+");
-		
-		// TODO: Finish this method.  Remember the countSyllables method from 
+
+
+
+		int lastPuncIndex = -1;
+		int lastWordIndex = -1;
+
+		for( int i=0; i<tokens.size(); i++ ) {
+			String token = tokens.get(i);
+
+			if( isWord(token) ) {
+				numWords++;
+				numSyllables += countSyllables(token);
+				lastWordIndex = i;
+			}
+			else {
+				numSentences++;
+				lastPuncIndex = i;
+			}
+		}
+
+		if( lastPuncIndex < lastWordIndex ) {
+			numSentences++;
+		}
+
+		display(tokens);
+		// TODO: Finish this method.  Remember the countSyllables method from
 		// Document.  That will come in handy here.
 	}
-	
-	
+
+	private void display( List<String> tokens ) {
+		System.out.println("Text is: ");
+		System.out.println(this.getText());
+
+		System.out.println();
+		System.out.println("Tokens are: ");
+		for( String token : tokens ) {
+			System.out.println(token);
+		}
+
+		System.out.println("Num words: " + numWords);
+		System.out.println("Num syllables: " + numSyllables);
+		System.out.println("Num sentences: " + numSentences);
+	}
+
 	/**
 	 * Get the number of words in the document.
 	 * "Words" are defined as contiguous strings of alphabetic characters
 	 * i.e. any upper or lower case characters a-z or A-Z
-	 * 
+	 *
 	 * @return The number of words in the document.
 	 */
 	@Override
@@ -63,10 +101,10 @@ public class EfficientDocument extends Document {
 
 	/**
 	 * Get the number of sentences in the document.
-	 * Sentences are defined as contiguous strings of characters ending in an 
-	 * end of sentence punctuation (. ! or ?) or the last contiguous set of 
+	 * Sentences are defined as contiguous strings of characters ending in an
+	 * end of sentence punctuation (. ! or ?) or the last contiguous set of
 	 * characters in the document, even if they don't end with a punctuation mark.
-	 * 
+	 *
 	 * @return The number of sentences in the document.
 	 */
 	@Override
@@ -78,8 +116,8 @@ public class EfficientDocument extends Document {
 	/**
 	 * Get the number of syllables in the document.
 	 * Words are defined as above.  Syllables are defined as:
-	 * a contiguous sequence of vowels, except for a lone "e" at the 
-	 * end of a word if the word has another set of contiguous vowels, 
+	 * a contiguous sequence of vowels, except for a lone "e" at the
+	 * end of a word if the word has another set of contiguous vowels,
 	 * makes up one syllable.   y is considered a vowel.
 	 * @return The number of syllables in the document.
 	 */
@@ -88,18 +126,20 @@ public class EfficientDocument extends Document {
         //TODO: write this method.  Hint: It's simple
         return 0;
 	}
-	
+
 	// Can be used for testing
 	// We encourage you to add your own tests here.
 	public static void main(String[] args)
 	{
+
+
 	    testCase(new EfficientDocument("This is a test.  How many???  "
                 + "Senteeeeeeeeeences are here... there should be 5!  Right?"),
                 16, 13, 5);
         testCase(new EfficientDocument(""), 0, 0, 0);
         testCase(new EfficientDocument("sentence, with, lots, of, commas.!  "
                 + "(And some poaren)).  The output is: 7.5."), 15, 11, 4);
-        testCase(new EfficientDocument("many???  Senteeeeeeeeeences are"), 6, 3, 2); 
+        testCase(new EfficientDocument("many???  Senteeeeeeeeeences are"), 6, 3, 2);
         testCase(new EfficientDocument("Here is a series of test sentences. Your program should "
 				+ "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
 				+ "the correct amount of syllables (example, for example), "
@@ -109,8 +149,14 @@ public class EfficientDocument extends Document {
 		testCase(new EfficientDocument("Sentences?!"), 3, 1, 1);
 		testCase(new EfficientDocument("Lorem ipsum dolor sit amet, qui ex choro quodsi moderatius, nam dolores explicari forensibus ad."),
 		         32, 15, 1);
-		
+
+
+
+		String text = "sentence, with, lots, of, commas.!";
+		EfficientDocument effDoc = new EfficientDocument(text);
+
+
 	}
-	
+
 
 }
