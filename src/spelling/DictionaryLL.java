beaf70a43cbcbfbb81a1722611371a83a2096f47
Pratyush Madhukar <pratyush.madhukar@gmail.com>
2016-02-27 17:54:05 +0530

    Implemented DictionaryLL.java All JUnit cases passed

--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -6,21 +6,29 @@ import java.util.LinkedList;
  * A class that implements the Dictionary interface using a LinkedList
  *
  */
-public class DictionaryLL implements Dictionary 
+public class DictionaryLL implements Dictionary
 {
 
 	private LinkedList<String> dict;
-	
+
     // TODO: Add a constructor
+	public DictionaryLL() {
+		dict = new LinkedList<String>();
+	}
 
 
     /** Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
      * @param word The word to add
-     * @return true if the word was added to the dictionary 
+     * @return true if the word was added to the dictionary
      * (it wasn't already there). */
     public boolean addWord(String word) {
     	// TODO: Implement this method
+    	word = word.toLowerCase();
+    	if( !dict.contains(word) ){
+    		dict.add(word);
+    		return true;
+    	}
         return false;
     }
 
@@ -29,14 +37,17 @@ public class DictionaryLL implements Dictionary
     public int size()
     {
         // TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
         //TODO: Implement this method
+    	if( dict.contains(s.toLowerCase()) ) {
+    		return true;
+    	}
         return false;
     }
 
-    
+
 }
