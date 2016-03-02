5b9416c71797b3070b34a6aee35bfc18fd6799b1
Pratyush Madhukar <pratyush.madhukar@gmail.com>
2016-03-02 23:55:47 +0530

    Dummy commit

--- src/spelling/NearbyWords.java
+++ src/spelling/NearbyWords.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package spelling;
 
@@ -17,17 +17,18 @@ public class NearbyWords implements SpellingSuggest {
 	// THRESHOLD to determine how many words to look through when looking
 	// for spelling suggestions (stops prohibitively long searching)
 	// For use in the Optional Optimization in Part 2.
-	private static final int THRESHOLD = 1000; 
+	private static final int THRESHOLD = 1000;
 
 	Dictionary dict;
 
-	public NearbyWords (Dictionary dict) 
+	public NearbyWords (Dictionary dict)
 	{
 		this.dict = dict;
 	}
 
+
 	/** Return the list of Strings that are one modification away
-	 * from the input string.  
+	 * from the input string.
 	 * @param s The original String
 	 * @param wordsOnly controls whether to return only words or any String
 	 * @return list of Strings which are nearby the original string
@@ -40,11 +41,11 @@ public class NearbyWords implements SpellingSuggest {
 		   return retList;
 	}
 
-	
+
 	/** Add to the currentList Strings that are one character mutation away
-	 * from the input string.  
+	 * from the input string.
 	 * @param s The original String
-	 * @param currentList is the list of words to append modified words 
+	 * @param currentList is the list of words to append modified words
 	 * @param wordsOnly controls whether to return only words or any String
 	 * @return
 	 */
@@ -52,14 +53,14 @@ public class NearbyWords implements SpellingSuggest {
 		// for each letter in the s and for all possible replacement characters
 		for(int index = 0; index < s.length(); index++){
 			for(int charCode = (int)'a'; charCode <= (int)'z'; charCode++) {
-				// use StringBuffer for an easy interface to permuting the 
+				// use StringBuffer for an easy interface to permuting the
 				// letters in the String
 				StringBuffer sb = new StringBuffer(s);
 				sb.setCharAt(index, (char)charCode);
 
 				// if the item isn't in the list, isn't the original string, and
 				// (if wordsOnly is true) is a real word, add to the list
-				if(!currentList.contains(sb.toString()) && 
+				if(!currentList.contains(sb.toString()) &&
 						(!wordsOnly||dict.isWord(sb.toString())) &&
 						!s.equals(sb.toString())) {
 					currentList.add(sb.toString());
@@ -67,22 +68,22 @@ public class NearbyWords implements SpellingSuggest {
 			}
 		}
 	}
-	
+
 	/** Add to the currentList Strings that are one character insertion away
-	 * from the input string.  
+	 * from the input string.
 	 * @param s The original String
-	 * @param currentList is the list of words to append modified words 
+	 * @param currentList is the list of words to append modified words
 	 * @param wordsOnly controls whether to return only words or any String
 	 * @return
 	 */
 	public void insertions(String s, List<String> currentList, boolean wordsOnly ) {
-		// TODO: Implement this method  
+		// TODO: Implement this method
 	}
 
 	/** Add to the currentList Strings that are one character deletion away
-	 * from the input string.  
+	 * from the input string.
 	 * @param s The original String
-	 * @param currentList is the list of words to append modified words 
+	 * @param currentList is the list of words to append modified words
 	 * @param wordsOnly controls whether to return only words or any String
 	 * @return
 	 */
@@ -91,9 +92,9 @@ public class NearbyWords implements SpellingSuggest {
 	}
 
 	/** Add to the currentList Strings that are one character deletion away
-	 * from the input string.  
+	 * from the input string.
 	 * @param word The misspelled word
-	 * @param numSuggestions is the maximum number of suggestions to return 
+	 * @param numSuggestions is the maximum number of suggestions to return
 	 * @return the list of spelling suggestions
 	 */
 	@Override
@@ -101,20 +102,20 @@ public class NearbyWords implements SpellingSuggest {
 
 		// initial variables
 		List<String> queue = new LinkedList<String>();     // String to explore
-		HashSet<String> visited = new HashSet<String>();   // to avoid exploring the same  
+		HashSet<String> visited = new HashSet<String>();   // to avoid exploring the same
 														   // string multiple times
 		List<String> retList = new LinkedList<String>();   // words to return
-		 
-		
+
+
 		// insert first node
 		queue.add(word);
 		visited.add(word);
-					
+
 		// TODO: Implement the remainder of this method, see assignment for algorithm
-		
+
 		return retList;
 
-	}	
+	}
 
    public static void main(String[] args) {
 	   /* basic testing code to get started
